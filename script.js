cab498efecb00aab4005e91f2bfe4254d234d114
Leonid Ratanov <leonid.ratanoff@gmail.com>
2019-10-31 23:29:40 +0500

    form to object

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 $(function () {
-    var update = function () {
+    let update = function () {
         $('#serializearray').text(
-            JSON.stringify($('form').serializeArray())
-        );
+            JSON.stringify(
+                $('form').serializeArray()
+                    .reduce(function (a, x) {
+                        a[x.name] = x.value;
+                        return a;
+                    }, {})
+            )
+        )
     };
+
     update();
     $('form').change(update);
-})
+});
